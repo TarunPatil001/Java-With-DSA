6a0782141a8f066328783ed925bad0a002e8f2c8
Tarun Patil <tarunpatil001@gmail.com>
2023-10-25 01:05:12 +0530

    Sorting the elements in ascending order in LinkedList using mergeSort.

--- 20. Divide & Conquer/DividenConquer.java
+++ 20. Divide & Conquer/DividenConquer.java
@@ -1,5 +1,61 @@
 public class DividenConquer {
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+
+        int mid = si + (ei - si) / 2; // (si+ei)/2;
+        mergeSort(arr, si, mid); // left sort;
+        mergeSort(arr, mid + 1, ei); // right sort;
+        merge(arr, si, mid, ei); // Both merge;
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        // [0-3] = 4 <--(3-0+1) [4-6] = 3 <--(6-4+1) (4+3) = 7 <--(6-0+1)
+        int temp[] = new int[ei - si + 1];
+        int i = si; // iterator for left part
+        int j = mid + 1; // iterator for right part
+        int k = 0; // iterator for temp arr
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) { // Comparision of left & right part.
+                temp[k] = arr[i];
+                i++;
+
+            } else {
+                temp[k] = arr[j]; // When only single element is there.
+                j++;
+            }
+            k++;
+        }
+
+        // left part
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // right part
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // Both parts are merge
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
     public static void main(String args[]) {
-        System.out.println("Hello");
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
--- 24. LinkedList/LinkedList.java
+++ 24. LinkedList/LinkedList.java
@@ -49,7 +49,7 @@ public class LinkedList {
 
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.data + " -> ");
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
         System.out.println("null");
@@ -280,6 +280,68 @@ public class LinkedList {
         prev.next = null;
     }
 
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private Node merge(Node head1, Node head2) {
+        Node mergedll = new Node(-1);
+        Node temp = mergedll;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergedll.next;
+
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // find mid
+        Node mid = getMid(head);
+
+        // left & right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        // merge
+        return merge(newLeft, newRight);
+    }
+
     public static void main(String args[]) {
         // LinkedList ll = new LinkedList();
         // ll.addFirst(2);
@@ -304,14 +366,25 @@ public class LinkedList {
         // System.out.println(ll.checkPalindrome());
         // ll.print();
 
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
+        // head = new Node(1);
+        // Node temp = new Node(2);
+        // head.next = temp;
+        // head.next.next = new Node(3);
+        // head.next.next.next = temp;
 
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+        // System.out.println(isCycle());
+        // removeCycle();
+        // System.out.println(isCycle());
+
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
 }
