d90295f6972fff6739f291515d7489f50df9b98b
Tarun Patil <tarunpatil001@gmail.com>
2023-10-31 11:36:59 +0530

    all changes are saved.

--- 24. LinkedList/LinkedList.java
+++ 24. LinkedList/LinkedList.java
@@ -342,6 +342,48 @@ public class LinkedList {
         return merge(newLeft, newRight);
     }
 
+    public void zigZag() {
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+
+        Node mid = slow;
+
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // alt merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+
+    }
+
     public static void main(String args[]) {
         // LinkedList ll = new LinkedList();
         // ll.addFirst(2);
@@ -376,15 +418,27 @@ public class LinkedList {
         // removeCycle();
         // System.out.println(isCycle());
 
+        // LinkedList ll = new LinkedList();
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+        // ll.addFirst(4);
+        // ll.addFirst(5);
+
+        // ll.print();
+        // ll.head = ll.mergeSort(ll.head);
+        // ll.print();
+
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addFirst(3);
-        ll.addFirst(4);
-        ll.addFirst(5);
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
 
         ll.print();
-        ll.head = ll.mergeSort(ll.head);
+        ll.zigZag();
         ll.print();
     }
 }
