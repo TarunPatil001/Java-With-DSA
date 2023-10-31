c263d1306f21f1ffe64fcd997fb79daa95d74dd6
Tarun Patil <tarunpatil001@gmail.com>
2023-11-01 01:45:10 +0530

    Q2. Reverse String using Stack.

--- 26. Stacks/StackB.java
+++ 26. Stacks/StackB.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class StackB {
 
     public static void pushAtBottom(Stack<Integer> s, int data) {
-        if (s.isEmpty()) {
+        // Using recursion method.
+        if (s.isEmpty()) { // checking whether stack is empty or not.
             s.push(data);
             return;
         }
@@ -12,17 +13,39 @@ public class StackB {
         s.push(top);
     }
 
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+
+        while (idx < str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder sb = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            sb.append(curr);
+        }
+
+        return sb.toString();
+    }
+
     public static void main(String args[]) {
 
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        pushAtBottom(s, 4);
+        // pushAtBottom(s, 4);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        // while (!s.isEmpty()) {
+        // System.out.println(s.pop());
+        // }
+
+        String str = "HelloWorld";
+        System.out.println(reverseString(str));
+
     }
 }
