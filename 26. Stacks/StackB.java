abda9bae5a78a9e63642b06ca4ceb30a2cad2b8f
Tarun Patil <tarunpatil001@gmail.com>
2023-11-23 14:37:41 +0530

    Q3. Stock Span Problem.

--- 26. Stacks/StackB.java
+++ 26. Stacks/StackB.java
@@ -31,6 +31,44 @@ public class StackB {
         return sb.toString();
     }
 
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+
+    public static void printStack(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void stockSpan(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>(); // stack created of integer type
+        span[0] = 1; // 0'th stock is always 1
+        s.push(0); // 0'th stock is pushed into stack
+
+        for (int i = 1; i < span.length; i++) { // loop will calculate span for each day & 0'th day we have already
+                                                // calculated so we will start i=1
+            int currPrice = stocks[i]; // To calculate span first we will calculate current price
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) { // comparing current price with previous prices
+                s.pop(); // removing previous smaller price stocks than currPrice stock
+            }
+            if (s.isEmpty()) { // while removing smaller elements checking whether stack is empty or not
+                span[i] = i + 1;
+            } else { // if it doesn't happen then...
+                int prevHigh = s.peek(); // previous high will the top element of stack
+                span[i] = i - prevHigh; // here we will get value of span
+            }
+
+            s.push(i); // value of span will be pused into the stack
+        }
+    }
+
     public static void main(String args[]) {
 
         // Stack<Integer> s = new Stack<>();
@@ -38,14 +76,19 @@ public class StackB {
         // s.push(2);
         // s.push(3);
 
-        // pushAtBottom(s, 4);
+        // reverseStack(s);
+        // printStack(s);
 
-        // while (!s.isEmpty()) {
-        // System.out.println(s.pop());
-        // }
+        // String str = "HelloWorld";
+        // System.out.println(reverseString(str));
 
-        String str = "HelloWorld";
-        System.out.println(reverseString(str));
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 }; // Stocks array created...
+        int span[] = new int[stocks.length]; // Span array created of same length as stocks...
+        stockSpan(stocks, span); // Function is called...
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.println(span[i] + " "); // stack value will be printed.
+        }
 
     }
 }
